72aad550e4762dc120dab86cb35f48178ff5c21d
KienPhan1109 <phantrungkien2006@gmail.com>
2024-12-09 10:53:03 +0700

    Add files via upload

---
+++ Session7-08.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int i, j, n, m;
+    printf("Nhập vào số hàng: "); scanf("%d", &n);
+    printf("Nhập vào số cột: "); scanf("%d", &m);
+    int arr[n][m];
+    printf("Nhập các phần tử của mảng:\n");
+    for ( i = 0 ; i < n ; i++) {
+        for ( j = 0 ; j < m ; j++) {
+            printf("Phần tử [%d][%d]: ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    printf("Mảng 2 chiều là:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
